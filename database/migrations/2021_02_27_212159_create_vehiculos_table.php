5d262572178453b3e82164fc3468099137e1267f
Nicolás Fracchia <nfracchia@hotmail.com>
2021-02-27 18:32:36 -0300

    Migraciones para las tablas de naves y vehiculos

---
+++ database/migrations/2021_02_27_212159_create_vehiculos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_swapi')->nullable(false);
+            $table->string('nombre')->nullable(false);
+            $table->string('modelo')->nullable(false);
+            $table->string('fabricante')->nullable(false);
+            $table->integer('unidades')->nullable(false)->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+}
